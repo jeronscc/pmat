80602cc6eb3ec787fa763336355337e11c4ac53a
Jeron <jeronscc@gmail.com>
2025-02-12 10:32:14 +0800

    fixed port

--- src/configs/connection.php
+++ src/configs/connection.php
@@ -1,12 +1,13 @@
 <?php
 
+// USER CONNECTION
 $servername = "localhost";
 $username = "root"; 
 $password = ""; 
 $database = "user"; 
 
 
-$conn_user = new mysqli($servername, $username, $password, $database, 3307);
+$conn_user = new mysqli($servername, $username, $password, $database);
 
 if ($conn_user->connect_error) {
     die("Connection failed: " . $conn_user->connect_error);
