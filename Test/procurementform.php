f2fd8cbe7a610abc079769e801c561628a8bff82
iameang <126791023+iameang@users.noreply.github.com>
2025-02-12 10:05:38 +0800

    edit

--- Test/procurementform.php
+++ Test/procurementform.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../src/css/mainheader.css"> 
-    <link rel="stylesheet" href="../src/Test/procurementform.css">
+    <link rel="stylesheet" href="procurementform.css">
     <?php include '../src/components/ilcdb_header.html'; ?>
 </head>
 <body>
