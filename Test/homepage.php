66c7713c1fe0db478835f246e2db3f7ecbac64f4
iameang <126791023+iameang@users.noreply.github.com>
2025-02-12 16:08:11 +0800

    hometophp

--- Test/homepage.php
+++ Test/homepage.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="../src/css/mainheader.css">
+    <?php include '../src/components/ilcdb_header.html'; ?>
 </head>
 <body>
     
