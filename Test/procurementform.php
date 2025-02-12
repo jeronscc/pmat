4ce5791ade3a37b9a3b62b332618edf1396f000b
iameang <126791023+iameang@users.noreply.github.com>
2025-02-12 09:36:34 +0800

    edit

--- Test/procurementform.php
+++ Test/procurementform.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Procurement Tracking and Monitoring System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="procurementform.css">
+    <link rel="stylesheet" href="../src/Test/procurementform.css">
     <link rel="stylesheet" href="../src/css/mainheader.css">
     <?php include '../src/components/'ilcdb_header.html'; ?>
 </head>
