d707278ff9b52a36899fd2174e9d7bfb761db557
iameang <126791023+iameang@users.noreply.github.com>
2025-02-13 14:53:27 +0800

    fltr

--- Test/homepage.php
+++ Test/homepage.php
@@ -24,7 +24,7 @@
 
         <div class="d-flex justify-content-end align-items-center mb-2">
             <div class="dropdown me-2">
-                <button class="icon-button dropdown-toggle" type="button" id="yearDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="dropdown-toggle" type="button" id="yearDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="../src/assets/filter.png" alt="Select Year" style="width: 20px; height: 20px; margin-right: 10px;">
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="yearDropdown">
