ff9b44aaab7c617a78d564de2517cf92680b1dd0
iameang <126791023+iameang@users.noreply.github.com>
2025-02-18 15:25:05 +0800

    add db for form

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ Route::get('/fetch-saro-ilcdb', function () {
     // Return data as JSON
     return response()->json($data);
 });
+
+// ILCDB FETCH HONORARIACHECKLIST
+Route::get('/fetch-honorariachecklist', function () {
+    // Fetch data from the honorariachecklist table in the ilcdb database
+    $data = DB::connection('requirements')->table('honorariachecklist')->get();
+
+    // Return data as JSON
+    return response()->json($data);
+});
