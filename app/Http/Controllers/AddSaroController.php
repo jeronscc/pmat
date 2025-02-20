c2e1244a9988095ba2958c65b6a53b491bd2b27e
Jeron Cansanay <jeronscc@gmail.com>
2025-02-20 14:37:01 +0800

    fixed saro controller

--- app/Http/Controllers/AddSaroController.php
+++ app/Http/Controllers/AddSaroController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class SaroController extends Controller
+class AddSaroController extends Controller
 {
     public function addSaro(Request $request)
     {
