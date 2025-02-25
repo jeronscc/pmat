4a3a4d9b7e44a4366341fabf6736fdb7716c8e8d
Jeron Cansanay <jeronscc@gmail.com>
2025-02-25 10:35:44 +0800

    fixed filter saro

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -926,6 +926,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error fetching SARO data:', error));
 });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
