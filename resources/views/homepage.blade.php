e0500c3ad19293a99d4f59da6d8939110015569f
Jeron Cansanay <jeronscc@gmail.com>
2025-02-25 13:18:05 +0800

    fixed filter again

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -263,7 +263,7 @@
                 </div>
             </div>
         </div>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
         <!-- Custom JS -->
         <script src="/js/menu.js"></script>
