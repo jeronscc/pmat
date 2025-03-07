b13931c122aebf61b1bc2c90de8aa9557e2eac35
EmmanuelCharlieEnriquez <21-03267@g.batstate-u.edu.ph>
2025-03-07 13:21:32 +0800

    selection css

--- resources/views/selection.blade.php
+++ resources/views/selection.blade.php
@@ -42,7 +42,7 @@
         </div>
         <div class="col-md-6 p-3">
             <div class="box">
-                <img src="/assets/click-logo.png" alt="PROJECT CLICK" class="img-fluid">
+                <img src="/assets/project_click_logo.png" alt="PROJECT CLICK" class="img-fluid">
             </div>
         </div>
     </div>
