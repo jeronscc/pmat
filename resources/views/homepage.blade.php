caf0b660e0a1c65bcdec0d9cb40fcc1f8e9d69a4
iameang <126791023+iameang@users.noreply.github.com>
2025-03-10 14:08:45 +0800

    show pr

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -348,7 +348,6 @@
         <script src="/js/addSaro.js"></script>
         <script src="/js/filterSaroByYear.js"></script>
         <script src="/sarohover.js"></script>
-        <script src="/js/filterTabs.js"></script>
 
         <!-- Bootstrap JS (Optional, only needed for dropdowns, modals, etc.) -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
