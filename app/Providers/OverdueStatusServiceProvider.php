396dda103c4b73421b9d62570ff8d09b50beb40e
Fenrir-qt <mickburog87@gmail.com>
2025-03-10 14:32:29 +0800

    Adjusted overdue interval (for testing)

--- app/Providers/OverdueStatusServiceProvider.php
+++ app/Providers/OverdueStatusServiceProvider.php
@@ -15,12 +15,12 @@ class OverdueStatusServiceProvider extends ServiceProvider
         if (app()->bound('db')) {
             DB::connection('ilcdb')->table('procurement_form')
                 ->whereRaw("
-                    (dt_received1 IS NULL AND dt_submitted1 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted1, INTERVAL 1 DAY))
-                    OR (dt_received2 IS NULL AND dt_submitted2 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted2, INTERVAL 1 DAY))
-                    OR (dt_received3 IS NULL AND dt_submitted3 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted3, INTERVAL 1 DAY))
-                    OR (dt_received4 IS NULL AND dt_submitted4 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted4, INTERVAL 1 DAY))
-                    OR (dt_received5 IS NULL AND dt_submitted5 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted5, INTERVAL 1 DAY))
-                    OR (dt_received6 IS NULL AND dt_submitted6 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted6, INTERVAL 1 DAY))
+                    (dt_received1 IS NULL AND dt_submitted1 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted1, INTERVAL 1 MINUTE))
+                    OR (dt_received2 IS NULL AND dt_submitted2 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted2, INTERVAL 1 MINUTE))
+                    OR (dt_received3 IS NULL AND dt_submitted3 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted3, INTERVAL 1 MINUTE))
+                    OR (dt_received4 IS NULL AND dt_submitted4 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted4, INTERVAL 1 MINUTE))
+                    OR (dt_received5 IS NULL AND dt_submitted5 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted5, INTERVAL 1 MINUTE))
+                    OR (dt_received6 IS NULL AND dt_submitted6 IS NOT NULL AND NOW() > DATE_ADD(dt_submitted6, INTERVAL 1 MINUTE))
                 ")
                 ->update(['status' => 'Overdue']);
         }
