b8d6dd842315269e0c426c336551809a243c8a73
Jjerrold <jerroldramirez15@gmail.com>
2025-03-21 18:10:35 +0800

    fix login error for newly added account

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,33 +4,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        // Validate the incoming request fields
         $incomingFields = $request->validate([
             'username' => 'required|string',
             'password' => 'required'
         ]);
-
-        // Attempt to find the user by username
+    
+        // Find user by username
         $user = DB::table('user_accs')->where('username', $incomingFields['username'])->first();
-
-        // Check if the user exists and if the password matches
-        if ($user && $user->password === $incomingFields['password']) {
-            // Log in the user
+    
+        // Check if user exists and password matches
+        if ($user && Hash::check($incomingFields['password'], $user->password)) {
             Auth::loginUsingId($user->user_id);
-            
-            // Regenerate the session to prevent session fixation attacks
             $request->session()->regenerate();
-
-            // Redirect the user to the homepage or their intended destination
+    
             return redirect()->intended('/select-project');
         } else {
-    // If the credentials are invalid, pass a custom error message to the session
-    return redirect()->back()->with('error', 'Invalid username or password');
+            return redirect()->back()->with('error', 'Invalid username or password');
         }
     }
 
