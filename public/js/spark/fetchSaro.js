1dc8bfe0ca6f1b778d52c43583d1d3a8fb101d91
iameang <126791023+iameang@users.noreply.github.com>
2025-04-02 14:03:56 +0800

    fix filter on spark and click

--- public/js/spark/fetchSaro.js
+++ public/js/spark/fetchSaro.js
@@ -149,7 +149,7 @@ function fetchNTCAForSaro(saroNo) {
                     const currentQuarter = getCurrentQuarter(ntca);
 
                     // Update NTCA container label and balance for the current quarter
-                    ntcaLabelElement.textContent = `NTCA (${ntca.ntca_no} - ${currentQuarter ? currentQuarter.replace('_q', ' Quarter') : 'No Quarter'})`;
+                    ntcaLabelElement.textContent = `NTCA (${ntca.ntca_no} | ${currentQuarter ? currentQuarter : 'No Quarter'})`;
                     const currentQuarterBalance = currentQuarter ? ntca[currentQuarter] : 0;
                     ntcaBalanceElement.textContent = currentQuarterBalance
                         ? `₱${currentQuarterBalance.toLocaleString()}`
