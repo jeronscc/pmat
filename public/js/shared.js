5410c7b3c5ebed0ce64dcbb3884bda5b6b68e359
iameang <126791023+iameang@users.noreply.github.com>
2025-04-04 09:03:58 +0800

    .

--- public/js/shared.js
+++ public/js/shared.js
@@ -265,12 +265,6 @@ function fetchNTCAForSaro(saroNo, ntcaApiUrl) {
                     // Add NTCA breakdown to the list
                     ntcaList.innerHTML += `
                     <li class="list-group-item d-flex justify-content-between">
-                        <strong>SARO Budget Allocated:</strong>
-                        <span class="fw-bold">
-                            ${ntca.saro_budget ? "₱" + ntca.saro_budget.toLocaleString() : "<em style='color:#777;'>Not yet allocated</em>"}
-                        </span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between">
                         <strong>NTCA Budget Allocated:</strong>
                         <span class="fw-bold">
                             ${ntca.ntca_budget ? "₱" + ntca.ntca_budget.toLocaleString() : "<em style='color:#777;'>Not yet allocated</em>"}
